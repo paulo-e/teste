965c4a9972557331a0b80cde7a8617b058198cde
Paulo Elienay <elienay.albuquerque@gmail.com>
2021-06-02 07:54:46 -0700

    docs(index.php): adiciona descrições sobre o tema `teste`

---
+++ index.php
@@ -0,0 +1,10 @@
+/*
+Theme Name: Teste
+Author: Paulo Elienay II
+Author URI: paulo-e.github.io
+Description: Tema teste para seleção
+Version: 1
+License: BSD 3 Clause License
+License URI: https://opensource.org/licenses/BSD-3-Clause
+Tags: teste, filmes
+*/
