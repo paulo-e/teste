c182ffb41745d1b8229ef2f2a2a4dfec02fa5102
Paulo Elienay <elienay.albuquerque@gmail.com>
2021-06-02 12:36:21 -0700

    refactor: move tags do `footer` e `header` para respectivos arquivos `.php`

--- front-page.php
+++ front-page.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html><!--  Last Published: Thu Dec 03 2020 14:57:07 GMT+0000 (Coordinated Universal Time)  -->
-<html data-wf-page="5fa45342033e748d877aad37" data-wf-site="5fa45342033e749f917aad36">
-<head>
-  <?php wp_head(); ?>
-</head>
-<body>
-  <div data-collapse="medium" data-animation="default" data-duration="400" id="Navigation" role="banner" class="nav-bar-v2 w-nav">
-    <a href="#" class="nav-brand-v2 w-nav-brand"><img src="images/escolas-mem.png" sizes="(max-width: 479px) 78.96875px, 105.296875px" srcset="images/escolas-mem-p-500.png 500w, images/escolas-mem-p-800.png 800w, images/escolas-mem-p-1080.png 1080w, images/escolas-mem.png 1111w" alt="" class="nav-logo"></a>
-    <div class="nav-space"></div>
-    <nav role="navigation" class="nav-menu-v2 w-nav-menu">
-      <form action="/search" class="search-form w-form"><input type="search" class="search-form-input w-input" maxlength="256" name="query" placeholder=" Search..." id="search" required=""><input type="submit" value=" " class="search-button w-button"></form>
-      <a href="#" class="nav-link w-nav-link">Home</a>
-      <a href="#" class="nav-link w-nav-link">Notícias</a>
-      <a href="#" class="nav-link w-nav-link">Matérias</a>
-      <a href="#" class="nav-link w-nav-link">Filmes</a>
-      <a href="#" class="nav-link w-nav-link">Podcasts</a>
-    </nav>
-    <div class="nav-right-v2">
-      <a href="#" class="button subscribe-button w-button">Receba Mimos</a>
-    </div>
-    <div class="menu-button-v2 w-nav-button">
-      <div class="menu-icon">
-        <div class="menu-line-top"></div>
-        <div class="menu-line-middle"></div>
-        <div class="menu-line-bottom"></div>
-      </div>
-    </div>
-    <div data-w-id="b6a32dce-ecac-cd1e-d4b6-2cef1b7a197d" class="menu-button-v2 side-menu-button">
-      <div class="menu-icon">
-        <div class="menu-line-top-2"></div>
-        <div class="menu-line-middle-2"></div>
-        <div class="menu-line-bottom-2"></div>
-      </div>
-    </div>
-    <div class="side-menu">
-      <div class="full-height-menu-contents">
-        <a href="#" class="w-inline-block"><img src="images/logo-mem2x.png" width="200" sizes="200px" srcset="images/logo-mem2x-p-500.png 500w, images/logo-mem2x.png 523w" alt=""></a>
-        <div class="full-height-menu-links">
-          <a href="#" class="full-height-menu-link">Home</a>
-          <a href="https://spark-template.webflow.io/#Landing-Pages" class="full-height-menu-link">Notícias</a>
-          <a href="#" class="full-height-menu-link">Matérias</a>
-          <a href="#" class="full-height-menu-link">Podcasts</a>
-          <a href="#" class="full-height-menu-link">Mimos</a>
-          <a href="#" class="full-height-menu-link">Contato</a>
-        </div>
-        <div class="full-height-menu-footer"></div>
-      </div>
-    </div>
-    <div class="side-menu-overlay"></div>
-  </div>
-  <div class="section top-section">
+<?php get_header(); ?>
+<div class="section top-section">
     <div class="wrapper">
       <div data-animation="slide" data-duration="600" data-infinite="1" id="Hero" class="slider-v2 w-slider">
         <div class="w-slider-mask">
@@ -592,62 +543,7 @@
       </div>
     </div>
   </div>
-  <div class="footer-3">
-    <div class="main-container">
-      <div class="footer-1-top">
-        <div class="main-container">
-          <div class="footer-1-top-row">
-            <a href="#" class="footer-1-logo w-inline-block"><img src="images/logo-mem2x.png" sizes="250px" srcset="images/logo-mem2x-p-500.png 500w, images/logo-mem2x.png 523w" alt="" class="image-7 logo"></a>
-            <ul role="list" class="social-icons-list w-list-unstyled">
-              <li class="list-item">
-                <a href="https://www.linkedin.com/company/santos-silva-gest%C3%A3o-empresarial/" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace3f980c72ea_linkedin.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://www.instagram.com/santossilvagestaoempresarial/" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace17750c7313_instagram.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://www.facebook.com/SantosSilvaGestaoEmpresarial" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace3df40c72ce_facebook.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://api.whatsapp.com/send?phone=+5521976499451&amp;text=Ol%C3%A1%21%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es." target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="images/whatsapp.svg" alt="" class="social-icon-2 social-icon-dark iconwhatsapp"></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="footer-3-top-row">
-        <div class="footer-3-logo-and-menus">
-          <div class="w-layout-grid menu-grid-vertical">
-            <a href="https://spark-template.webflow.io/#Landing-Pages" class="hover-link text-white text-large">Início</a>
-            <a href="https://spark-template.webflow.io/#Company-Pages" class="hover-link text-white text-large">Notícias</a>
-            <a href="https://spark-template.webflow.io/#Pricing-Pages" class="hover-link text-white text-large">Matérias</a>
-            <a href="https://spark-template.webflow.io/#Account-Pages" class="hover-link text-white text-large">Podcasts</a>
-            <a href="https://spark-template.webflow.io/#Account-Pages" class="hover-link text-white text-large mimos">Mimos? Temos!</a>
-          </div>
-        </div>
-        <div class="container">
-          <h5 class="footer-form-heading">Contato</h5>
-          <p class="paragraph-2">(00) 0000-0000<br>falecom@mem.com.br<br><br><strong>Matriz<br></strong>Avenida nome da rua, 000 - Bairro, Cidade - UF, 00000-000</p>
-          <div class="div-block-4">
-            <a href="https://cursos.uniterset.org/ver/curso/formacao-de-auditores-de-responsabilidade-social/" target="_blank" class="button-6 small bt-borda bt-footer w-inline-block">
-              <div class="text-block-21 whtas-txt-2 txt-menu-button text-block-22 bt-footer">Querio anunciar neste site!</div>
-            </a>
-            <a href="https://cursos.uniterset.org/ver/curso/formacao-de-auditores-de-responsabilidade-social/" target="_blank" class="button-6 small bt-borda w-inline-block">
-              <div class="text-block-21 whtas-txt-2 txt-menu-button text-block-22">Contato</div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="footer-bottom-row footer-3-bottom-row">
-        <div class="footer-bottom-links">
-          <a href="#" class="row-link text-white">Políticas legais</a>
-          <div>© Criado por <a href="https://www.webflow.com/" target="_blank" class="hover-link text-white">Visualpage</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <?php wp_footer(); ?>
+  <?php get_footer(); ?>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
