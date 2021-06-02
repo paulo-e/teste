bbcd0cb8708e63596a868cb3aa1ff036ef83bf2b
Paulo Elienay <elienay.albuquerque@gmail.com>
2021-06-02 09:01:11 -0700

    feat(index.php): adiciona `get_header()` que importa `header.php`
    adiciona `language_attributes()` em `<html>`

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-/*
-Theme Name: Teste
-Author: Paulo Elienay II
-Author URI: paulo-e.github.io
-Description: Tema teste para seleção
-Version: 1
-License: BSD 3 Clause License
-License URI: https://opensource.org/licenses/BSD-3-Clause
-Tags: teste, filmes
-*/
+<?php
+
+get_header();
+
+?>
