0056aa59c92284f9a766e48e385041a14346fa4f
Paulo Elienay <elienay.albuquerque@gmail.com>
2021-06-02 11:29:26 -0700

    feat: adiciona registro de scripts js com caminhos temporarios

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <html data-wf-page="5fa45342033e748d877aad37" data-wf-site="5fa45342033e749f917aad36">
 <head>
   <meta charset="utf-8">
-  <title>MEM</title>
+  <title><?php bloginfo('name'); ?></title>
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <?php wp_head(); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
@@ -656,8 +656,6 @@
       </div>
     </div>
   </div>
-  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5fa45342033e749f917aad36" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="js/mem-c1ad69.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -9,4 +9,14 @@ function teste_register_styles () {
 
 add_action('wp_enqueue_scripts', "teste_register_styles");
 
+function teste_register_scripts () {
+    // FIXME
+
+    wp_enqueue_script("jquery", "https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5fa45342033e749f917aad36", array(), '3.5.1');
+    wp_enqueue_script("mem-c1ad69", get_template_directory_uri() . "/theme-template/js/mem-c1ad69.js", array(), '1');
+}
+
+add_action('wp_enqueue_scripts', "teste_register_scripts");
+
+
 ?>
